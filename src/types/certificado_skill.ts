1dbea038d738a08ba213bbd4a96064e5b00de302
A01198539 <a01198539@tec.mx>
2025-05-08 00:28:41 -0600

    feat(types): certificado_skill
    add file

---
+++ src/types/certificado_skill.ts
@@ -0,0 +1,13 @@
+export interface CertificadoSkill {
+    id_certificado_skill: number;
+    id_certificado: number;
+    id_skill: number;
+    created_at?: string;
+    updated_at?: string;
+  }
+  
+  export interface CertificadoSkillInput {
+    id_certificado: number;
+    id_skill: number;
+  }
+  

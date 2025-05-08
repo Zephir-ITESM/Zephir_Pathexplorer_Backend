4802e7ef14c424b6be92bd114c79c72a060f6a1d
YolisJC <a01285390@tec.mx>
2025-05-08 00:11:12 -0600

    chore(controllers): imports
    add imports without creating functions

---
+++ src/controllers/skillsController.ts
@@ -0,0 +1,6 @@
+import type { Request, Response } from "express"
+import jwt from "jsonwebtoken"
+import { UserModel } from "../models/userModels"
+import { SkillModel } from "../models/skillModel"
+import { validateUserRegistration, validateUserLogin } from "../utils/validation"
+import e from "express"
